94e795b3bdbd07b050eafcdc7a217946c43f4eec
DecimTheJudge <paolo1piga@gmail.com>
2024-06-16 18:50:37 +0200

    inizio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// creo un array con vari oggetti al suo interno e creo un let con la lista della spesa
+let listArray = [`Uova`, `Olio`, `Burro`,`Pane`, `Acqua`, `Pasta`];
+let lista = document.getElementById(`lista-spesa`); 
+
+// creerò un while che passi per tutti gli oggetti dell'array
+
