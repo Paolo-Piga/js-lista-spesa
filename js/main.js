52d59d9bfa7292424418e1a56fd74b9d31f2ea00
DecimTheJudge <paolo1piga@gmail.com>
2024-06-16 18:55:30 +0200

    finito il while

--- js/main.js
+++ js/main.js
@@ -2,5 +2,17 @@
 let listArray = [`Uova`, `Olio`, `Burro`,`Pane`, `Acqua`, `Pasta`];
 let lista = document.getElementById(`lista-spesa`); 
 
+// creo un "i" a 0 per prenderlo come base per il while
+let i = 0;
 // creerò un while che passi per tutti gli oggetti dell'array
+while (i < lista-spesa.length){
+    // creo gli elementi li 
+    let listaLi = document.createElement(`li`);
 
+    // creo un "textContent" per aggiungere ogni oggetto dentro un li
+    listaLi.textContent = listArray[i];
+
+    // aggiungo i li all'ul con un "appendChild"
+    lista.appendChild(lista);
+    i++;
+}
